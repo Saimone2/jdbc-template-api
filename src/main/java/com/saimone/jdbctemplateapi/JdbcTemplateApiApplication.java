2f42a4b1b691c164283f63c7fff748e0a848b96e
Andriy Khrushch <csgo.reserv@gmail.com>
2023-06-26 13:56:42 +0300

    init

---
+++ src/main/java/com/saimone/jdbctemplateapi/JdbcTemplateApiApplication.java
@@ -0,0 +1,13 @@
+package com.saimone.jdbctemplateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTemplateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saimone/jdbctemplateapi/JdbcTemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimone.jdbctemplateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
